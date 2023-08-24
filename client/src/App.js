d58f4887bcdd36322850c4f37780883fc9af43f4
ksudharshan1706 <kamavaramsudharshan1706@gmail.com>
2023-08-24 11:26:20 +0530

    aigpt 1st commit

---
+++ client/src/App.js
@@ -0,0 +1,42 @@
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+// import CssBaseline from "@mui/material/CssBaseline";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "./theme.js";
+import Navbar from "./components/Navbar";
+import Register from "./pages/Register";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Summary from "./pages/Summary";
+import Paragraph from "./pages/Paragraph";
+import ChatBot from "./pages/ChatBot";
+import JsConverter from "./pages/JsConverter";
+import ScifiImage from "./pages/ScifiImage";
+import DisplayPage from "./components/DisplayPage";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings(), []));
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/summary" element={<Summary />} />
+          <Route path="/paragraph" element={<Paragraph />} />
+          <Route path="/chatbot" element={<ChatBot />} />
+          <Route path="/js-converter" element={<JsConverter />} />
+          <Route path="/scifi-image" element={<ScifiImage />} />
+          <Route path="/about" element={<DisplayPage />} />
+        </Routes>
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
